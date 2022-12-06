bf0aed34d69aeb09ce97f49758a74f3595cf233d
modernpacifist <modernpacifist@gmail.com>
2022-12-06 20:43:28 +0300

    beta main with fastapi

---
+++ main.py
@@ -0,0 +1,44 @@
+# import whisper
+# import streamlit as st
+
+# st.title("Whisper App")
+
+# # upload audio file with streamlit
+# audio_file = st.file_uploader("Upload Audio", type=["wav", "mp3", "m4a"])
+
+# model = whisper.load_model("base")
+# st.text("Whisper Model Loaded")
+
+# st.text("Play Original Audio File")
+# st.audio(audio_file)
+
+# if st.button("Transcribe Audio"):
+    # if audio_file is None:
+        # st.error("Please upload an audio file")
+        # exit(1)
+
+    # st.success("Transcribing Audio")
+    # transcription = model.transcribe(audio_file.name, fp16=False, language='ru')
+    # st.success("Transcription Complete")
+    # st.markdown(transcription["text"])
+
+
+import whisper
+import requests as rq
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/translate/")
+async def create_file(url: str = None):
+    model = whisper.load_model("base")
+
+    doc = rq.get(url)
+    with open('voice.mp3', 'wb') as f:
+        f.write(doc.content)
+        result = model.transcribe("voice.mp3", fp16=False, language='ru')
+
+    return {"result": result["text"]}
+
