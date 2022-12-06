8c1b6808281c847d52fcaa82bd1e21f61c951565
Maksembek <56928893+Maksembek@users.noreply.github.com>
2022-12-06 19:42:29 +0300

    starting

---
+++ mainWisper.py
@@ -0,0 +1,22 @@
+import whisper
+import streamlit as st
+
+st.title("Whisper App")
+
+# upload audio file with streamlit
+audio_file = st.file_uploader("Upload Audio", type=["wav", "mp3", "m4a"])
+
+model = whisper.load_model("base")
+st.text("Whisper Model Loaded")
+
+st.text("Play Original Audio File")
+st.audio(audio_file)
+
+if st.button("Transcribe Audio"):
+    if audio_file is not None:
+        st.success("Transcribing Audio")
+        transcription = model.transcribe(audio_file.name, fp16=False, language='ru')
+        st.success("Transcription Complete")
+        st.markdown(transcription["text"])
+    else:
+        st.error("Please upload an audio file")
